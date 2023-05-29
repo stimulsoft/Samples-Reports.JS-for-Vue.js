34e57aa3f926e4fe74167697c77da3d8eed61edc
Uladzimir Khotchanka <hotchenko@gmail.com>
2023-05-29 17:36:02 +0200

    add sample projects

---
+++ Vue.js/Integrating the Report Designer into an Application/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <div>
+      <h2>Stimulsoft Reports.JS Designer</h2>
+      <div id="designer"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data () {
+    return {
+    
+    }
+  },
+  mounted: function() {
+    console.log('Loading Designer view');
+
+    console.log('Set full screen mode for the designer');
+    var options = new window.Stimulsoft.Designer.StiDesignerOptions();
+    options.appearance.fullScreenMode = false;
+
+    console.log('Create the report designer with specified options');
+    var designer = new window.Stimulsoft.Designer.StiDesigner(options, 'StiDesigner', false);
+
+    console.log('Create a new report instance');
+    var report = new window.Stimulsoft.Report.StiReport();
+
+    console.log('Load report from url');
+    report.loadFile('reports/SimpleList.mrt');
+
+    console.log('Edit report template in the designer');
+    designer.report = report;
+
+    console.log('Rendering the viewer to selected element');
+    designer.renderHtml('designer');
+
+    console.log('Loading completed successfully!');
+  }
+}
+</script>
+
+<style>
+</style>
